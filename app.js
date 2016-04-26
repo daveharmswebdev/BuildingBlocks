335698e04a9a9936e17919f99fd10017a8f1aab7
Dave Harms <daveharmswebdev@gmail.com>
2016-04-26 08:51:58 -0500

    added index.html and using static and added patients end point

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-	response.send('Hello Dave!');
+app.use(express.static('public'));
+
+app.get('/patients', function(request, response) {
+	var patients = ['Harms, David','Peeples, Trevor','Doyle, Josh'];
+	response.json(patients);
 });
 
 module.exports = app;
+
+// left off at 35.47 just finished making index.html and using express static
--- test.js
+++ test.js
@@ -6,15 +6,62 @@ describe('Requests to the root path', function() {
 
 	it('Returns a 200 status code', function(done) {
 
-	request(app)
-		.get('/')
-		.expect(200)
-		.end(function(error) {
-			if(error) throw error;
-			done();
-		});
+		request(app)
+			.get('/')
+			.expect(200, done);
 
 	});
 
+	it('Returns a HTML format', function(done) {
+		request(app)
+			.get('/')
+			.expect('Content-Type', /html/, done);
+	});
+
+	it('Returns an index file with patients', function(done) {
+		request(app)
+			.get('/')
+			.expect(/patients/i, done);
+	});
+
 });
+
+describe('Listing patients on /patients', function() {
+
+	it('Returns 200 status code', function(done) {
+
+		request(app)
+			.get('/patients')
+			.expect(200, done);
+
+	});
+
+	it('Returns JSON format', function(done) {
+
+		request(app)
+			.get('/patients')
+			.expect('Content-Type', /json/, done);
+
+	});
+
+	it('Returns initial patients', function(done) {
+
+		request(app)
+			.get('/patients')
+			.expect(JSON.stringify(['Harms, David','Peeples, Trevor','Doyle, Josh']), done);
+
+	});
+	
+});
+
+
+// describe('', function() {
+
+// 	it('', function(done) {
+
+// 	});
+
+// });
+
+// finished at 23:00 about to write second test.
 
